c7b1aebe55271c5d7aa1c620e2cbef0fc4200105
RubensHerculano22 <rubensherculano@hotmail.com>
2023-03-26 11:59:21 -0300

    Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,52 +1,15 @@
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
-    <!-- {{valor}}
-    <button (click)="adicionar()">Adicionar</button> -->
-    <app-title *ngIf="destruir"></app-title>
-    <br/>
-    <button (click)="destruirComponent()">Destruir Componente</button>
+    <app-data-biding></app-data-biding>
     <router-outlet></router-outlet>
   `
 })
-export class AppComponent implements OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked{
-  //public title = 'Bem Vindo'; Variavel para mostrar na tela(app.component.html)
-  // public valor: number = 1;
-
-  public destruir:boolean = true;
-
+export class AppComponent implements OnInit{
   constructor() {}
 
-  // public adicionar(): number {
-  //   return this.valor += 1;
-  // }
-
   ngOnInit(): void {
-  }
-
-  public destruirComponent() {
-    this.destruir = false;
-  }
-
-  ngDoCheck(): void {
-    // console.log("doCheck")
-  }
-
-  ngAfterContentInit(): void {
-    // console.log("afterContentInit")
-  }
-
-  ngAfterContentChecked(): void {
-    // console.log("aftercontentChecked")
-  }
-
-  ngAfterViewInit(): void {
-    // console.log("afterViewInit")
-  }
-
-  ngAfterViewChecked(): void {
-    // console.log("afterViewChecked")
   }
 }
---
+++ src/app/data-biding/data-biding.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-biding',
+  templateUrl: './data-biding.component.html',
+  styleUrls: ['./data-biding.component.scss']
+})
+export class DataBidingComponent implements OnInit{
+  
+  public nome:string = "Rhysand";
+  public idade: number = 514;
+  
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+}
