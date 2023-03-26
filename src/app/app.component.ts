f395a9adebc2fa239a51d017806c1591f0b2617e
RubensHerculano22 <rubensherculano@hotmail.com>
2023-03-26 14:03:49 -0300

    pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-root',
   template: `
     <!-- <app-diretivas-estruturais></app-diretivas-estruturais> -->
-    <app-diretivas-atributos>
+    <!-- <app-diretivas-atributos>
       <h1>Aulas de Diretivas de Atributos</h1>
       <h3>Final da Aula </h3>
       <hr>
@@ -15,7 +15,8 @@ import { Component, OnInit } from '@angular/core';
       <h3>Acowar</h3>
       <hr>
     </app-diretivas-atributos>
-    <router-outlet></router-outlet>
+    <router-outlet></router-outlet> -->
+    <app-diretivas-atributos></app-diretivas-atributos>
   `
 })
 export class AppComponent implements OnInit{
