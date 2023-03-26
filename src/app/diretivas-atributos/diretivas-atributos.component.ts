2dbda9d9dd06dc4d806dcfa07397aa7f482b7be1
RubensHerculano22 <rubensherculano@hotmail.com>
2023-03-26 13:30:57 -0300

    ngClass

--- src/app/diretivas-atributos/diretivas-atributos.component.ts
+++ src/app/diretivas-atributos/diretivas-atributos.component.ts
@@ -8,16 +8,27 @@ import { Component, OnInit } from '@angular/core';
 export class DiretivasAtributosComponent implements OnInit{
 
   public valor: boolean = true;
+  public height: string = "20px";
+  public backgroundColor: string = "blue";
   constructor() { }
 
   ngOnInit(): void {
-    
+
     setInterval( () => {
       if(this.valor) {
         this.valor = false;
       } else {
         this.valor = true;
       }
+
+      if(this.height == "20px") {
+        this.height = "50px"
+        this.backgroundColor = "red";
+      } else {
+        this.height = "20px"
+        this.backgroundColor = "blue";
+      }
+
     }, 2000);
   }
 }
