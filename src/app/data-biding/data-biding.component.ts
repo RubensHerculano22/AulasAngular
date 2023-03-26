e1a50ff881139a807f2cfcefbfce885c369ee031
RubensHerculano22 <rubensherculano@hotmail.com>
2023-03-26 12:09:04 -0300

    Property Binding

--- src/app/data-biding/data-biding.component.ts
+++ src/app/data-biding/data-biding.component.ts
@@ -9,7 +9,12 @@ export class DataBidingComponent implements OnInit{
   
   public nome:string = "Rhysand";
   public idade: number = 514;
+  public maisUm: number = 1;
   
+  public checkedDisabled:boolean = false;
+  public imgScr: string = "https://images2.alphacoders.com/279/279072.jpg";
+  public imgTitle: string = "Mario Party";
+
   constructor() {
 
   }
