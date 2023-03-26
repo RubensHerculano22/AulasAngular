193a823cc92ede002484da22e5b244f3e0acd6b9
RubensHerculano22 <rubensherculano@hotmail.com>
2023-03-26 11:51:03 -0300

    On Ddestory

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,44 +3,50 @@ import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit,
 @Component({
   selector: 'app-root',
   template: `
-    {{valor}}
-    <button (click)="adicionar()">Adicionar</button>
+    <!-- {{valor}}
+    <button (click)="adicionar()">Adicionar</button> -->
+    <app-title *ngIf="destruir"></app-title>
+    <br/>
+    <button (click)="destruirComponent()">Destruir Componente</button>
     <router-outlet></router-outlet>
   `
 })
 export class AppComponent implements OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked{
   //public title = 'Bem Vindo'; Variavel para mostrar na tela(app.component.html)
-  public valor: number = 1;
+  // public valor: number = 1;
+
+  public destruir:boolean = true;
+
   constructor() {}
 
-  public adicionar(): number {
-    return this.valor += 1;
-  }
+  // public adicionar(): number {
+  //   return this.valor += 1;
+  // }
 
   ngOnInit(): void {
-    // setTimeout(() => {
-    //   console.log(1)
-    // }, 5000);
-    //console.log(1);
+  }
+
+  public destruirComponent() {
+    this.destruir = false;
   }
 
   ngDoCheck(): void {
-    console.log("doCheck")
+    // console.log("doCheck")
   }
 
   ngAfterContentInit(): void {
-    console.log("doCheck")
+    // console.log("afterContentInit")
   }
 
   ngAfterContentChecked(): void {
-    console.log("aftercontentChecked")
+    // console.log("aftercontentChecked")
   }
 
   ngAfterViewInit(): void {
-    console.log("afterViewInit")
+    // console.log("afterViewInit")
   }
 
   ngAfterViewChecked(): void {
-    console.log("afterViewChecked")
+    // console.log("afterViewChecked")
   }
 }
