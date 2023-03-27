f65ac395a522479e55e7af4a3d3056add6641b65
RubensHerculano22 <rubensherculano@hotmail.com>
2023-03-27 20:35:24 -0300

    Injeção de Dependecia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,11 +22,13 @@ import { Component, OnInit } from '@angular/core';
     <!-- <app-input [contador]="addValue"></app-input>
     <button (click)="add()">Add</button> -->
 
-    <ng-template [ngIf]="getDados">
+    <!-- <ng-template [ngIf]="getDados">
       <h1>{{getDados.nome}}</h1>
       <h2>{{getDados.idade}}</h2>
     </ng-template>
-    <app-output (enviarDados)="setDados($event)"></app-output>
+    <app-output (enviarDados)="setDados($event)"></app-output> -->
+
+    <app-food-list></app-food-list>
     <router-outlet></router-outlet>
   `
 })
