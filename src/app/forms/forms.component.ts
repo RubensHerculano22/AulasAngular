230bf902049f7ac4b5194fa879797ae5a3139f05
RubensHerculano22 <rubensherculano@hotmail.com>
2023-03-27 22:06:55 -0300

    ngForm Select

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forms.component.scss']
 })
 export class FormsComponent implements OnInit{
+
+  public listComidas: Array<{ comida: string, preco: string }> = [
+    {comida: 'Alface', preco: 'R$10,00'},
+    {comida: 'Tomate', preco: 'R$12,00'},
+    {comida: 'Batata', preco: 'R$14,00'},
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
