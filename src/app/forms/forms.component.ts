8f7728f20d6f4801d96dbb968e42d6395112bd55
RubensHerculano22 <rubensherculano@hotmail.com>
2023-03-27 22:19:31 -0300

    ngSubmit

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
@@ -17,5 +18,11 @@ export class FormsComponent implements OnInit{
 
   ngOnInit(): void {
     
+  }
+
+  public submitForm(form: NgForm){
+    if(form.valid) {
+      console.log(form.value);
+    }
   }
 }
